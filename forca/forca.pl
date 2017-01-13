03895ebc85d0b959504d0771f7bbb833d49834b0
Lino Cesar <linux06@hotmail.com>
2017-01-13 00:10:25 -0300

    Jogo da forca - producao

--- forca/forca.pl
+++ forca/forca.pl
@@ -11,16 +11,102 @@ my $arquivo = 'palavras.txt'; #$ARGV[0];
 my $numero_de_palavras; #numero_de_palavras no arquivo.txt
 my $palavra_numero; #numero da linha onde se encontra a palavra.
 my $palavra_sorteada; #palavra
-my $numero_jogadores;
-my @jogador;
-
-
-
+my $numero_jogadores; #quantidade jogadores
+my @jogador;#lista armazena nome dos jogadores;
+my @letras;
+my $tamanho_palavra;
+my $contotal = 0;
+my $i = 0;
+my $palpite;
+my $tamanho_palpite;
+my $log_palpites = "";
+my $indice;
+my $relatorio;
+my $vencedor;
+my $flag = 0;
 
 $numero_jogadores = &setNumeroDeJogadores;
 @jogador = &iniciaJogadores($numero_jogadores);
+$numero_de_palavras = &contaPalavras($arquivo);
+$palavra_numero = int rand($numero_de_palavras) + 1;
+$palavra_sorteada = &pegaPalavra($arquivo, $palavra_numero);
+$tamanho_palavra = length $palavra_sorteada;
+@letras = split("", $palavra_sorteada);
 
+$relatorio = &imprimeSaida($tamanho_palavra);
+print "A PALAVRA TEM ".$tamanho_palavra. " LETRAS\n";
+print $relatorio."\n";
 
+do{
+
+  print "QUAL SEU PALPITE, ".$jogador[$i]."?"."\n";
+  $palpite = uc(<STDIN>);
+  chomp($palpite);
+  $tamanho_palpite = length $palpite;
+
+  if ($tamanho_palpite == $tamanho_palavra)
+  {
+    if($palpite eq $palavra_sorteada)
+    {
+      $vencedor = $jogador[$i];
+      $flag = 1;
+    }
+  }elsif($tamanho_palpite == 1)
+  {
+    $indice = index($log_palpites, $palpite);
+    $log_palpites .= $palpite;
+
+      if($indice == -1)
+        {
+            $relatorio = &insereLetraPalavra($palpite, $palavra_sorteada, $relatorio);
+              if($relatorio eq $palavra_sorteada){
+                  $vencedor = $jogador[$i];
+                  $flag = 1;
+              }
+        }else{print RED, "LETRA JA ESCOLHIDA\n", RESET;}
+
+   }else {print RED, "APENAS UMA LETRA OU PALAVRA\n", RESET;}
+
+  if ($flag == 0) {
+    print $relatorio."\n";
+  }
+
+  $contotal++;
+  $i = $contotal % $numero_jogadores;
+
+}while($flag == 0);
+
+print $vencedor . " VENCEU!\n";
+
+sub insereLetraPalavra {
+  my $letra = $_[0]; #palpite de uma letra
+  my $sorte = $_[1]; #palavra sorteada
+  my $aux = $_[2];
+  my @lista_sorte = split("", $sorte);
+  my @lista_aux = split("", $aux);
+  my $saida = "";
+
+  for (my $q = 0; $q < length $sorte; $q++) {
+    if($letra eq $lista_sorte[$q])
+    {
+        $lista_aux[$q] = $letra;
+    }
+    $saida .= $lista_aux[$q];
+  }
+
+  return $saida;
+
+}
+
+sub imprimeSaida {
+  my $num = $_[0];
+  my $out;
+  for (my $q = 0; $q < $num; $q++) {
+    $out .= "#";
+  }
+  return $out;
+
+}
 
 sub setNumeroDeJogadores {
   my $numero;
@@ -41,48 +127,6 @@ sub setNumeroDeJogadores {
   return $numero;
 
 }
-
-
-
-
-
-$numero_de_palavras = &contaPalavras($arquivo);
-$palavra_numero = int rand($numero_de_palavras) + 1;
-$palavra_sorteada = &pegaPalavra($arquivo, $palavra_numero);
-#print $numero_de_palavras."\n";
-#print $palavra_numero."\n";
-#print $palavra_sorteada."\n";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 sub contaPalavras {
 
