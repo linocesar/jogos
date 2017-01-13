7c8d4b00787b0c4ccc05a1173a5ae15cf65224bf
Lino Cesar <linux06@hotmail.com>
2017-01-13 00:12:14 -0300

    Jogo da forca - producao

--- forca/teste.pl
+++ forca/teste.pl
@@ -4,10 +4,18 @@ use Term::ANSIColor qw(:constants);
 print RED, "Stop!\n", RESET;
 print GREEN, "Go!\n", RESET;
 
+my $num;
+for (my $t = 0; $t < 10; $t++) {
+  $num = $t % 3 + 1;
+  print $num;
+}
+print "\n";
+my $m = index("aads", "q") ;
+print  $m . "\n";
 
-my $t = <STDIN>;
-chomp($t);
 
-if ($t == '[a-zA-Z]') {
+my $nomea = "CASA";
+
+if ($nomea eq "C#SA") {
   print "ok\n";
 }
